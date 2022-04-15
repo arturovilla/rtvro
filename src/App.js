64ba3af9aeea8d6c7339e3d96befbf5f0851b05f
arturovilla <acvturi@gmail.com>
2022-04-15 01:33:11 -0500

    added routing feature in project with react router dom v6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import "./App.css";
+import "./css/App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Portfolio from "./Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey bitch from app.js</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hey bitch from app.js</h1>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+    </Router>
   );
 }
 
