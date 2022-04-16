f6e112c44b32e216e5d96894194bcd5f1f8723ca
arturovilla <acvturi@gmail.com>
2022-04-15 20:56:39 -0500

    add new css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
+import { Email, Twitter, LinkedIn, GitHub } from "@mui/icons-material";
+import { Box } from "@mui/material";
 import React from "react";
 import "./css/Footer.css";
 function Footer() {
-  return <div className="Footer">Footer with links and shit</div>;
+  return (
+    <div className="Footer">
+      Footer with links and shit
+      <Box className="Iconbox">
+        <LinkedIn className="Icons_" />
+        <GitHub className="Icons_" />
+        <Email className="Icons_" />
+        <Twitter className="Icons_" />
+      </Box>
+    </div>
+  );
 }
 
 export default Footer;
