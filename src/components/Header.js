7ffd7a39552264f3c49a6b48568dc9ee2d6fb062
arturovilla <acvturi@gmail.com>
2022-04-16 02:28:48 -0500

    continously reshaping navbar to be complient with focus conventions for buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Toolbar } from "@mui/material";
 import "./css/Header.css";
 
 function Header() {
   return (
-    <Box className="Header_box">
-      <AppBar position="static" className="Bar_">
-        <Toolbar>
-          <Typography>NAVBAR SHIT</Typography>
-        </Toolbar>
+    <Box className="box1">
+      <AppBar position="static" className="appb">
+        <Box className="box2">
+          <Toolbar className="toolb">
+            <Button className="toolbutton" variant="outlined">
+              image
+            </Button>
+
+            <Box className="box3">
+              <Button className="toolbutton" variant="outlined">
+                Home
+              </Button>
+
+              <Button className="toolbutton" variant="outlined">
+                Skills
+              </Button>
+
+              <Button className="toolbutton" variant="outlined">
+                Work
+              </Button>
+            </Box>
+          </Toolbar>
+        </Box>
       </AppBar>
     </Box>
   );
